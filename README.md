129e294fa5f3c0d1f88d4bcb0552944781af4d4e
George Paterson <paterson.george@gmail.com>
2015-01-27 15:07:55 +0000

    Adding better comments.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# jQuery dialog plugin
+# jQuery plugin
 
-Demonstration jQuery dialog plugin showing how to structure a plugin with public functions for unit testing.
+Demonstration jQuery plugin showing how to structure a plugin with public functions for unit testing.
 
 _Dialog plugin is only designed as a demonstration not for production._
 
 ## Getting Started
 
-Download the [production version][min] or the [development version][max].
+Download the [minified version][min] or the [development version][max].
 
 [min]: https://raw.githubusercontent.com/georgepaterson/jquery-jquery-plugin/master/dist/jquery.jquery-plugin.min.js
 [max]: https://raw.githubusercontent.com/georgepaterson/jquery-jquery-plugin/master/dist/jquery.jquery-plugin.js
@@ -27,10 +27,9 @@ jQuery(function($) {
 ```
 
 ## Documentation
-_(Coming soon)_
 
-## Examples
-_(Coming soon)_
+This jQuery plugin demonstrates a jQuery plugin pattern with public functions, supporting structured unit testing.
+
 
 ## Release History
 _(Nothing yet)_
--- src/jquery-plugin.js
+++ src/jquery-plugin.js
@@ -7,71 +7,77 @@
  */
 (function ($) {
   'use strict';
+  // Dialog class constructor.
 	var Dialog = function (element, options) {
 		this.element = $(element);
 		this.options = options;
     this.isShown = null;
 	};
+  // Dialog show public function.
   Dialog.prototype.show = function () {
     var that = this;
-    /*  */
+    // If dialog is already shown, exit.
     if (this.isShown) {
       return;
     }
-    /*  */
-    this.element.addClass('dialog-show').attr('aria-hidden', false).trigger('focus');    
+    // Add class and ARIA attributes, set focus on dialog. 
+    this.element.addClass('dialog-show').attr('aria-hidden', false).trigger('focus');
+    // Add modal background, add hide event trigger.    
     this.modal = $('<div class="dialog-modal" />').prependTo(this.element).on('click.dialog', function (event) {
       that.hide();
     });
-    /*  */
+    // Add focusin event, if in page focus is not on the dialog set focus on the dialog.
     $(document).on('focusin.dialog', function (event) {
       if (that.element[0] !== event.target && !that.element.has(event.target).length) {
         that.element.trigger('focus');
       }
     });
-    /*  */
+    // Add keyup event, if escape key is used trigger hide event.
 		$(document).on('keyup.dialog', function (event) {
 			if (event.which === 27) {
 				that.hide();
 			}
 		});
-    /*  */
+    // Dialog is now shown.
     this.isShown = true;
   };
+  // Dialog hide public function.
   Dialog.prototype.hide = function () {
-    /*  */
+    // If dialog is not shown, exit.
     if (!this.isShown) {
       return;
     }
-    /*  */
+    // Remove class and ARIA attributes.
     this.element.removeClass('dialog-show').attr('aria-hidden', true);
-    /*  */
+    // Remove modal background.
     this.modal.remove();
-    /*  */
+    // Clear modal value.
     this.modal = null;
-    /*  */
+    // Remove focusin event.
     $(document).off('focusin.dialog');
-    /*  */
+    // Remove keyup event.
     this.element.off('keyup.dialog');
-    /*  */
+    // Dialog is now not shown.
     this.isShown = false;
   };
+  // Dialog destroy public function. 
   Dialog.prototype.destroy = function () {
-    /*  */
+    // Trigger the hide public function 
     this.hide();
-    /*  */
+    // Remove the dialog data attached to the element.
     this.element.removeData('dialog');
   };
+  // Dialog jQuery plugin instantiation.
   $.fn.dialog = function (method) {
     return this.each(function () {
-      /*  */
+      // Set data and options.
 			var data = $(this).data('dialog'),
 				options = $.extend(true, {}, $.fn.dialog.defaults, $(this).data('dialog'), typeof method === 'object' && method);
-			/*  */
+			// If no data create a new dialog.
       if (!data) {
 				$(this).data('dialog', (data = new Dialog(this, options)));
 			}
-      /*  */
+      // Test if a public function is called, else if no function try showing the dialog.
 			if (typeof method === 'string' && data[method]) {
 				data[method]();
 			} else if (options.auto) {
@@ -79,7 +85,7 @@
 			}
     });
   };
-  /*  */
+  // Dialog default options.
   $.fn.dialog.defaults = {
     auto: true
   };
